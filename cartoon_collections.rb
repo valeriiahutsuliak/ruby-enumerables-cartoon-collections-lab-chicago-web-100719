372f0fbb4441282d7387035bb13c3a034858729d
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 21:28:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,4 +2,5 @@ def roll_call_dwarves(dwarf_array)
   dwarf_array.each_with_index { |item, index| puts "#{index + 1} #{item}" }
 end	
 def summon_captain_planet(call_array)
-  
+  call_array.map { |item| "#{item.capitalize}!" }
+end
